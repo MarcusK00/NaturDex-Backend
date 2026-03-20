cf7f5ee470d5625984c58ed618a84e25cdbbdd21
Marcus <marcuskristensen10@hotmail.com>
2026-03-20 08:48:52 +0100

    Filled Animal model with Data.

--- NaturDex.Core/Models/Animal.cs
+++ NaturDex.Core/Models/Animal.cs
@@ -8,5 +8,13 @@ namespace NaturDex.Core.Models
 {
     public class Animal
     {
+        public int Id { get; set; }
+        public string? Spieces { get; set; }
+        public string? ScientificName { get; set; }
+        public string? Description { get; set; }
+        public string? Region { get; set; }
+        public string? DietDetails { get; set; }
+        public float AverageWeightKg { get; set; }
+        public int AverageLifeSpan { get; set; }
     }
 }
