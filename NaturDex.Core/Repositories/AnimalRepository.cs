2327f5c65db303d909a420467615581c064899cc
Marcus <marcuskristensen10@hotmail.com>
2026-03-20 09:38:55 +0100

    Repository methods: UpdateAsync, GetByIdAsync and GetallAsync is now done. Everything should work as intended.

--- NaturDex.Core/Repositories/AnimalRepository.cs
+++ NaturDex.Core/Repositories/AnimalRepository.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using NaturDex.Core.Models;
 
 using NaturDex.Core.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace NaturDex.Core.Repositories
 {
@@ -49,19 +50,51 @@ namespace NaturDex.Core.Repositories
             catch (Exception ex) { throw new InvalidOperationException("Failed to delete animal.", ex);}
         }
 
-        public Task<List<Animal>> GetAllAsync()
+        public async Task<List<Animal>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+              return await _dbContext.Animals.ToListAsync();
+            } catch (DbUpdateException ex){ throw new InvalidOperationException("Failed to retrieve animals.", ex); }
         }
 
-        public Task<Animal> GetByIdAsync(int id)
+        public async Task<Animal> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            if (id <= 0) throw new ArgumentException("Id must be greater than 0", nameof(id));
+
+            Animal? animal = await _dbContext.Animals.FindAsync(id);
+
+            if (animal == null) throw new InvalidOperationException($"Failed to retrieve animal with id: {id}");
+
+            return animal;
         }
 
-        public Task<Animal> UpdateAsync(Animal entity)
+        public async Task<Animal> UpdateAsync(Animal animal)
         {
-            throw new NotImplementedException();
+            if (animal == null)
+                throw new ArgumentNullException(nameof(animal));
+
+            if (animal.Id <= 0)
+                throw new ArgumentException("Animal Id must be greater than 0.", nameof(animal.Id));
+
+            var existingAnimal = await _dbContext.Animals.FindAsync(animal.Id);
+
+            if (existingAnimal == null) throw new InvalidOperationException($"Animal with id {animal.Id} not found.");
+
+            existingAnimal.Spieces = animal.Spieces;
+            existingAnimal.ScientificName = animal.ScientificName;
+            existingAnimal.Description = animal.Description;
+            existingAnimal.Region = animal.Region;
+            existingAnimal.DietDetails = animal.DietDetails;
+            existingAnimal.AverageWeightKg = animal.AverageWeightKg;
+            existingAnimal.AverageLifespan = animal.AverageLifespan;
+
+            try
+            {
+              await _dbContext.SaveChangesAsync();
+            } catch (DbUpdateException ex) { throw new InvalidOperationException("Failed to update animal.", ex); }
+            
+            return existingAnimal;
         }
     }
 }
