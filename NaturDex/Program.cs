ae0bd45cdacb56c99f9e0b623ed9dd670ac1d5fd
Marcus <marcuskristensen10@hotmail.com>
2026-03-20 10:31:44 +0100

    Made all Minimal Api endpoints following REST principles. Created two GET enpoints, a POST, PUT & DELETE. Ready to get hosted.

--- NaturDex/Program.cs
+++ NaturDex/Program.cs
@@ -1,16 +1,21 @@
+using Microsoft.EntityFrameworkCore;
 using NaturDex.Core.Interfaces;
 using NaturDex.Core.Models;
 using NaturDex.Core.Repositories;
+using System;
+using NaturDex.Core.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? Environment.GetEnvironmentVariable("DATABASE_URL");
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<NaturDexDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddScoped<IRepository<Animal>, AnimalRepository>();
+
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
@@ -21,6 +26,99 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//API ENDPOINTS
+app.MapGet("/api/v1/animals/{id}", async (int id, IRepository<Animal> repo) =>
+{
+    try
+    {
+        if (id <= 0)
+            return Results.BadRequest("Invalid animal ID");
+
+        var animal = await repo.GetByIdAsync(id);
+
+        if (animal is null)
+            return Results.NotFound($"Animal with ID {id} not found");
+
+        return Results.Ok(animal);
+    }
+    catch (Exception ex)
+    {
+        return Results.InternalServerError(ex);
+    }
+});
+
+app.MapGet("/api/v1/animals", async (IRepository<Animal> repo) =>
+{
+    try
+    {
+
+        var animals = await repo.GetAllAsync();
+        
+        if (animals == null || animals.Count == 0)
+            return Results.NotFound("No animals found");
+
+        return Results.Ok(animals);
+    }
+    catch (Exception ex)
+    {
+        return Results.InternalServerError(ex);
+    }
+});
+
+app.MapPost("/api/v1/animals", async (Animal animal, IRepository<Animal> repo) =>
+{
+    try
+    {
+        if (animal is null)
+            return Results.BadRequest("Animal is required");
+
+        if (string.IsNullOrWhiteSpace(animal.Species))
+            return Results.BadRequest("Animal name is required");
+
+        var createdAnimal = await repo.CreateAsync(animal);
+        return Results.Created($"/api/v1/animals/{createdAnimal.Id}", createdAnimal);
+    }
+    catch (Exception ex)
+    {
+        return Results.InternalServerError(ex);
+    }
+});
+
+app.MapPut("/api/v1/animals/{id}", async (int id, Animal animal, IRepository<Animal> repo) =>
+{
+    try
+    {
+        if (animal is null) 
+            return Results.BadRequest("Animal is required");
+
+        animal.Id = id;
+        var updatedAnimal = await repo.UpdateAsync(animal);
+
+        return Results.Ok(updatedAnimal); 
+    }
+    catch (Exception ex)
+    {
+        return Results.InternalServerError(ex);
+    }
+});
+
+app.MapDelete("/api/v1/animals/{id}", async (int id, IRepository<Animal> repo) =>
+{
+    try
+    {
+        if (id <= 0) 
+            return Results.BadRequest("Id must be over 0");
+
+        await repo.DeleteAsync(id);
+
+        return Results.NoContent();
+    }
+    catch (Exception ex)
+    {
+        return Results.InternalServerError(ex);
+    }
+});
 
 app.UseAuthorization();
 
