3a155f06c15e34909aa1685bdd86b775770240e2
Marcus <marcuskristensen10@hotmail.com>
2026-03-20 08:43:11 +0100

    Renamed DbContext to fit project title.

---
+++ NaturDex.Core/Data/NaturDexDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NaturDex.Core.Models;
+
+
+namespace NaturDex.Core.Data
+{
+    public class NaturDexDbContext : DbContext
+    {
+        public DbSet<Animal> Animals { get; set; }
+
+        public NaturDexDbContext(DbContextOptions<NaturDexDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
