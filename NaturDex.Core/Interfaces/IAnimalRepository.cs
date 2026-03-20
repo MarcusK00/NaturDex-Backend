59266727212dbb236ed2a110f3d52093ec812d56
Marcus <marcuskristensen10@hotmail.com>
2026-03-20 08:55:13 +0100

    Created  IAnimalRepository methods. Ready to be implemented in AnimalRepository.

--- NaturDex.Core/Interfaces/IAnimalRepository.cs
+++ NaturDex.Core/Interfaces/IAnimalRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NaturDex.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace NaturDex.Core.Interfaces
 {
-    public interface IAnimalRepository
+    public interface IAnimalRepository<T> where T : class
     {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> UpdateAsync(T entity);
+        Task<T> CreateNewAsync(T entity);
+        Task DeleteAsync(int id);
+
     }
 }
