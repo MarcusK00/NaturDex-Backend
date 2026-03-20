068c02095628195d0c7b76cf5f3bae1393fdbde0
Marcus <marcuskristensen10@hotmail.com>
2026-03-20 08:36:52 +0100

    Created folders for Data, Interfaces, Models and Repos in .Core project.

---
+++ NaturDex.Core/Interfaces/IAnimalRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturDex.Core.Interfaces
+{
+    internal class IAnimalRepository
+    {
+    }
+}
