e08b7e4beaeb53abb30ced0c62c170429509a1e5
Marcus <marcuskristensen10@hotmail.com>
2026-03-20 09:13:04 +0100

    Changed Repository interface to generic instead.

--- NaturDex.Core/Interfaces/IRepository.cs
+++ NaturDex.Core/Interfaces/IRepository.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace NaturDex.Core.Interfaces
 {
-    public interface IAnimalRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         Task<List<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
         Task<T> UpdateAsync(T entity);
-        Task<T> CreateNewAsync(T entity);
+        Task<T> CreateAsync(T entity);
         Task DeleteAsync(int id);
 
     }
