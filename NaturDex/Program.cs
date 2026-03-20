fffb92ee374bd1056829be0d5ebacf22c9a75bbd
Marcus <marcuskristensen10@hotmail.com>
2026-03-20 09:43:52 +0100

    Added repository to middleware.

--- NaturDex/Program.cs
+++ NaturDex/Program.cs
@@ -1,3 +1,7 @@
+using NaturDex.Core.Interfaces;
+using NaturDex.Core.Models;
+using NaturDex.Core.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IRepository<Animal>, AnimalRepository>();
 
 var app = builder.Build();
 
